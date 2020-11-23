287a7edf0a98ac9346a49882909c33a9cbea6ef3
Kyle McDowell <kymcd21@icstudents.org>
2020-11-23 22:22:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java
+class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    for (int i = 1; i <= 5; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println("");
+
+    }
+  }
+
+}
