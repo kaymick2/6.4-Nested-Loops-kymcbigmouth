b39f50fb42eb9d81fdd319b21f9086da170f35e5
Kyle McDowell <kymcd21@icstudents.org>
2020-11-24 16:31:50 +0000

    still doesn't run

--- Main.java
+++ Main.java
@@ -5,14 +5,13 @@ class Main {
     Scanner in = new Scanner(System.in);
     System.out.println("enter number of rows and smash that enter key:");
     int rows = in.nextInt();
-    int stars = rows;
     int count = 0;
     while (rows > 0) {
       for (int m = 1; m <= count; m++) {
-        System.out.println("");
+        System.out.print(" ");
       }
-      for (int k = 1; k <= (2 * x) - 1; k++) {
-        System.out.println("*");
+      for (int k = 1; k <= (2 * k) - 1; k++) {
+        System.out.print("*");
       }
 
       count++;
