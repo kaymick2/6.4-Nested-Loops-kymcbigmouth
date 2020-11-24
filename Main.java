17a7fe691bbac3175bfd5d059eb279dfe7228a4b
Kyle McDowell <kymcd21@icstudents.org>
2020-11-24 04:16:36 +0000

    isosceles triangle before pyramid code is added and most likely broken

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    for (int i = 1; i <= 5; i++) {
+    System.out.println("Enter the amount of rows you want and press enter:");
+
+    int rows=in.nextInt();
+    for (int i = 1; i <= rows; i++) {
       for (int j = 0; j < i; j++) {
         System.out.print("*");
       }
