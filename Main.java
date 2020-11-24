6f634acabb454674275d7c452296a0be32149539
Kyle McDowell <kymcd21@icstudents.org>
2020-11-24 16:33:45 +0000

    posibly runs?????

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ class Main {
     System.out.println("enter number of rows and smash that enter key:");
     int rows = in.nextInt();
     int count = 0;
-    while (rows > 0) {
+    for(int x=rows; x>=0;x--) {
       for (int m = 1; m <= count; m++) {
         System.out.print(" ");
       }
-      for (int k = 1; k <= (2 * k) - 1; k++) {
+      for (int k = 1; k <= (2 * x) - 1; k++) {
         System.out.print("*");
       }
 
